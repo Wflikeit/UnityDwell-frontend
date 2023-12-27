6b1d1edd3e795474b28ebb796955d3fa94c4bca3
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-27 14:56:30 +0100

    added initial routes and hanlding types for ts compiler

---
+++ src/types/routes.ts
@@ -0,0 +1,13 @@
+export enum AppRoutes {
+  HOME = '/publications',
+  BILLS = '/bills',
+  RESIDENTS = '/residents',
+  SETTINGS = '/settings',
+  BILLS_NEW = '/bills/new',
+  PUBLICATIONS_NEW = '/publications/new',
+  BUILDINGS_NEW = '/buildings/new',
+  BUILDINGS_EDIT = '/buildings/edit',
+  PUBLICATIONS_EDIT = '/publications/:id',
+  APARTMENTS_EDIT = '/apartments/:id',
+  LOGIN_PAGE = '/login',
+}
