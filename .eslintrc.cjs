b4935dffab6f1c009bad46dac542e653f28fba5f
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-28 16:26:35 +0100

    added stylelint and eslintignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,38 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
+  plugins: ['@typescript-eslint', 'react-hooks', 'unused-imports'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.tsx'] },
+    ],
+    "react/react-in-jsx-scope": "off",
+    'react/no-unused-prop-types': 'off',
+    'react/no-unescaped-entities': ['off', { allowHTML: true }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        caughtErrors: 'none',
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-declarations': 'off',
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
 }
--- src/pages/LoginPage/LoginForm.tsx
+++ src/pages/LoginPage/LoginForm.tsx
@@ -1,12 +1,10 @@
 import '/src/scss/loginPage.scss';
-import { Helmet } from 'react-helmet-async';
 import { Box } from '@mui/material';
 
 const LoginForm = () => {
   return (
     <>
-      <Box className="login__page__container">
-      </Box>
+      <Box className="login__page__container"/>
     </>
   );
 };
