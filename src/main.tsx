b4878df3ac6aa50b1ef669eea8ed7838660e67fe
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-28 22:02:51 +0100

    added basic login mechanism with auth0

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import './index.scss';
 import { HelmetProvider } from 'react-helmet-async';
 import {Auth0Provider} from '@auth0/auth0-react';
 
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const domain = import.meta.env.VITE_DOMAIN;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-    domain="{yourDomain}"
-    clientId="{yourClientId}"
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
--- src/pages/LoginPage/LoginForm.tsx
+++ src/pages/LoginPage/LoginForm.tsx
@@ -1,10 +1,22 @@
 import '/src/scss/loginPage.scss';
-import { Box } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginForm = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <>
-      <Box className="login__page__container"/>
+      <Box className="login__page__container">
+        <Typography variant="h2" className="welcoming__text">
+          {' '}
+          Welcome to
+        </Typography>
+        <Typography variant="h2" className="welcoming__text">
+          {' '}
+          UnityDwell
+        </Typography>
+        <Button onClick={() => loginWithRedirect()}>Sign in</Button>
+      </Box>
     </>
   );
 };
