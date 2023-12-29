32f6d142728b7786d162450097b2361cd953bd45
Wiktor Frątczak <wiktorft19@gmail.com>
2023-12-29 01:30:58 +0100

    added getting access token from auth0

--- src/pages/LoginPage/LoginForm.tsx
+++ src/pages/LoginPage/LoginForm.tsx
@@ -4,18 +4,26 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const LoginForm = () => {
   const { loginWithRedirect } = useAuth0();
+  const onClickAction = async () => {
+    await loginWithRedirect({
+      authorizationParams: {
+        redirect_uri: 'http://localhost:5173/publications',
+      },
+    });
+  };
+
   return (
     <>
       <Box className="login__page__container">
-        <Typography variant="h2" className="welcoming__text">
-          {' '}
+        <Typography variant="h3" className="welcoming__text">
           Welcome to
         </Typography>
-        <Typography variant="h2" className="welcoming__text">
-          {' '}
+        <Typography variant="h3" className="welcoming__text">
           UnityDwell
         </Typography>
-        <Button onClick={() => loginWithRedirect()}>Sign in</Button>
+        <Button id="sign__in__button" onClick={onClickAction}>
+          Sign in
+        </Button>
       </Box>
     </>
   );
