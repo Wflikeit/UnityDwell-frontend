b21880c0f0b14d0b34747568920b95064c6243f3
Wiktor Frątczak <wiktorft19@gmail.com>
2024-01-02 03:03:29 +0100

    fixed receiving valid access_token

--- src/pages/PublicationsPage/PublicationsPage.tsx
+++ src/pages/PublicationsPage/PublicationsPage.tsx
@@ -1,6 +1,9 @@
 import { Helmet } from 'react-helmet-async';
 import { useEffect } from "react";
 import {useAuth0} from '@auth0/auth0-react';
+import axios from 'axios';
+import {Button} from '@mui/material';
+import {AddressModel} from '../../models/Address.ts';
 
 const PublicationsPage = () => {
   const { getAccessTokenSilently } = useAuth0();
@@ -11,18 +14,31 @@ const PublicationsPage = () => {
       try {
         const accessToken = await getAccessTokenSilently();
         localStorage.setItem('access_token', accessToken);
+        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
       } catch (error) {
-        console.error('Błąd uzyskiwania tokena dostępu:', error);
+        console.error('Error accessing an access token:', error);
       }
-    };
 
+    };
     fetchData();
   }, [getAccessTokenSilently]);
+  const doSth = async (): Promise<AddressModel> => {
+    try {
+      const response = await axios.get<AddressModel>('http://localhost:8080/api/address/76e3d25f-2705-4fbb-97cc-8b78613e80b6', {
+      });
+      console.log(response);
+      return response.data;
+    } catch (error) {
+      console.error('Błąd podczas pobierania danych:', error);
+      throw error; // Dodaj ten fragment, aby błędy były przekazywane dalej
+    }
+  };
   return (
     <>
       <Helmet>
         <title>Publications</title>
       </Helmet>
+      <Button onClick={doSth}>Click</Button>
     </>
   );
 };
