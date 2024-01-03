8bcdcbe8122861dbc226c1f7fd3211b6a29a0cab
Wiktor Frątczak <wiktorft19@gmail.com>
2024-01-03 22:22:33 +0100

    added notFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,4 +1,10 @@
+import React from 'react';
+
 import { Helmet } from 'react-helmet-async';
+import Box from '@mui/material/Box/Box';
+
+import Typography from '@mui/material/Typography/Typography';
+
 
 const NotFoundPage = () => {
     return (
@@ -6,7 +12,21 @@ const NotFoundPage = () => {
             <Helmet>
                 <title>Not Found</title>
             </Helmet>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                    flexDirection: 'column',
+                }}
+            >
+                <Typography variant="h1">404 Not Found</Typography>
+                <a href="/publications">Home Page</a>
+            </Box>
         </>
     );
 };
 export default NotFoundPage;
+
+
